8ffa5f4da8705dffbaea83a944a855669d5b25a9
Mohammed Khan <abukhan363@gmail.com>
2024-04-30 23:29:29 -0400

    worked on approximating amplitude using hat basis function (Eqn 16)

--- src/interpolation.cpp
+++ src/interpolation.cpp
@@ -1,51 +1,51 @@
 #include "amplitude.h"
 
-// TODO: potentially fix this to reflect eqn 16 in the paper (one with basis funcs)
-double Amplitude::interpolateAmplitude4d(Vector2d x, double theta, double waveNumber) {
-    Vector2d idxSpacePos = posToIdxSpace(x);
-    double idxSpaceX = idxSpacePos.x();
-    double idxSpaceY = idxSpacePos.y();
-    double idxSpaceTheta = (float)dimTheta * theta / (2.0 * M_PI);
+// NOTE: interpolateAmplitude4d is rewritten in basis.cpp
+//double Amplitude::interpolateAmplitude4d(Vector2d x, double theta, double waveNumber) {
+//    Vector2d idxSpacePos = posToIdxSpace(x);
+//    double idxSpaceX = idxSpacePos.x();
+//    double idxSpaceY = idxSpacePos.y();
+//    double idxSpaceTheta = (float)dimTheta * theta / (2.0 * M_PI);
 
-    int idxXMin = floor(idxSpaceX);
-    int idxYMin = floor(idxSpaceY);
-    int idxThetaMin = floor(idxSpaceTheta);
+//    int idxXMin = floor(idxSpaceX);
+//    int idxYMin = floor(idxSpaceY);
+//    int idxThetaMin = floor(idxSpaceTheta);
 
-    std::vector<double> amplitudes;
-    std::vector<double> weights;
+//    std::vector<double> amplitudes;
+//    std::vector<double> weights;
 
-    // Doing a similar thing to what we discussed with amplitude interpolation for advection for now
-    // Aka: perform interpolation between the nearest 8 points
-    for (int i = idxXMin; i <= idxXMin + 1; i++) {
-        for (int j = idxYMin; j <= idxYMin + 1; j++) {
-            for (int k = idxThetaMin; k <= idxThetaMin + 1; k++) {
-                double amplitude = m_currentAmplitude.get(i, j, k, 0);
-                double weight = (Vector3d(i, j, k) - Vector3d(idxSpaceX, idxSpaceY, idxSpaceTheta)).norm();
-                amplitudes.push_back(amplitude);
-                weights.push_back(weight);
-            }
-        }
-    }
+//    // Doing a similar thing to what we discussed with amplitude interpolation for advection for now
+//    // Aka: perform interpolation between the nearest 8 points
+//    for (int i = idxXMin; i <= idxXMin + 1; i++) {
+//        for (int j = idxYMin; j <= idxYMin + 1; j++) {
+//            for (int k = idxThetaMin; k <= idxThetaMin + 1; k++) {
+//                double amplitude = m_currentAmplitude.get(i, j, k, 0);
+//                double weight = (Vector3d(i, j, k) - Vector3d(idxSpaceX, idxSpaceY, idxSpaceTheta)).norm();
+//                amplitudes.push_back(amplitude);
+//                weights.push_back(weight);
+//            }
+//        }
+//    }
 
-    double weightSum = 0;
-    for (double weight: weights) {
-        weightSum += weight;
-    }
-    for (double& weight: weights) {
-        weight /= weightSum;
-        assert(weight >= 0 && weight <= 1);
-    }
+//    double weightSum = 0;
+//    for (double weight: weights) {
+//        weightSum += weight;
+//    }
+//    for (double& weight: weights) {
+//        weight /= weightSum;
+//        assert(weight >= 0 && weight <= 1);
+//    }
 
-    double interpolatedAmplitude = 0;
-    for (int i = 0; i < amplitudes.size(); i++) {
-        interpolatedAmplitude += amplitudes[i] * weights[i];
-    }
+//    double interpolatedAmplitude = 0;
+//    for (int i = 0; i < amplitudes.size(); i++) {
+//        interpolatedAmplitude += amplitudes[i] * weights[i];
+//    }
 
-    // part that I'm least sure about but I'm not sure what the basis function for wave number is supposed to be besides this
-    //    interpolatedAmplitude *= m_profileBuffer.spectrum(m_profileBuffer.dispersion(waveNumber));
+//    // part that I'm least sure about but I'm not sure what the basis function for wave number is supposed to be besides this
+//    //    interpolatedAmplitude *= m_profileBuffer.spectrum(m_profileBuffer.dispersion(waveNumber));
 
-    return interpolatedAmplitude;
-}
+//    return interpolatedAmplitude;
+//}
 
 // TODO: turn this simple into cubic spatial interpolation instead of the simple adj amplitude samples weighted avg
 double Amplitude::interpolateAmplitude(Vector2d idxSpacePos, int thetaIdx) {
